4967df7ca76de912b13428303d11d327001fb2f8
luccadavid <david.silva@sptech.school>
2022-05-30 16:17:48 -0300

    porta

--- site/public/dashboard/main.js
+++ site/public/dashboard/main.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 
 
 const SERIAL_BAUD_RATE = 9600;
-const SERVIDOR_PORTA = 3000;
+const SERVIDOR_PORTA = 3300;
 const HABILITAR_OPERACAO_INSERIR = true;
 
 const serial = async (
